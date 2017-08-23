9309df0ff3aa8a7374bfd6bd28ff7529330df49d
Mercy Manrique <mercymanrique@yahoo.com>
2017-08-22 18:45:50 -0700

    ajax to index.js

--- index.js
+++ index.js
@@ -11,12 +11,13 @@ $.ajax({
    async:false,
     dataType: "json",
  success: function(data){
-  console.log(data[1][0]);
-  console.log(data[2][0]);
-  console.log(data[3][0]);
+  //console.log(data[1][0]);
+  //console.log(data[2][0]);
+  //console.log(data[3][0]);
+  $('#output').html('');
   for(var i=0;i<data[1].length;i++){
- $('#output').prepend("<li><a href= "+data[3][i]+">"[1][i] +"</a><p>"+data[2][i]+"</p></li>");
-}  
+ $('#output').prepend("<li><a href= "+data[3][i]+">"+data[1][i] +"</a><p>"+data[2][i]+"</p></li>");
+  }
  },
  error: function(errorMessage){
   alert("Error");
@@ -26,3 +27,4 @@ $.ajax({
 });
  });
   });
+ 
